d6d73b8ff7e67f96f2e31f18d956a3dbd9c4267c
sungsieon <131899323+sungsieon@users.noreply.github.com>
2023-04-27 02:46:29 +0900

    Add files via upload
    youtube

---
+++ 1.js
@@ -0,0 +1,12 @@
+
+let num = 0;
+$('.btn').on('click',function(){
+    num += 1
+    $('.number').html(num);
+});
+
+let num2 = 0;
+$('.btn2').on('click',function(){
+    num2 += 1
+    $('.number2').html(num2);
+});
